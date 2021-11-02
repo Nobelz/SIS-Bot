2f2ad34d3242c95bd83fa0d007bee152779963a7
Nobel Zhou <nobelzhou19@gmail.com>
2021-11-02 14:03:12 -0400

    Preparation for Spring 2022 semester registration cycle.
    * Fixed deprecation warnings due to new selenium update
    * Removed manual sleeping time in favor of WebDriverWait for shopping cart button
    * Fixed formatting and added exception handling in the case of wrong authentication

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import datetime
 import argparse
-import time
 
+import selenium.common.exceptions
 from dateutil import parser
 from getpass import getpass
-from selenium.webdriver import Chrome
+
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Set up commandline arguments
 help_text = "This selenium bot allows you to register for classes on the Case Western SIS portal right at 7:00 AM."
@@ -29,33 +33,39 @@ usernameStr = input("Please enter your Case ID (abc123): ")
 passwordStr = getpass("Please enter your password: ")
 
 # Start the Selenium WebDriver
-browser = Chrome()
+service = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service)
 browser.get('https://sis.case.edu/psc/P92SCWR_3/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV&AJAXTransfer=y&ICAJAXTrf=true&ICMDListSlideout=true')
 
 # Wait for and get username field
-WebDriverWait(browser, 10).until(lambda d: d.find_element_by_id('userid'))
-username = browser.find_element_by_id('userid')
+WebDriverWait(browser, 10).until(lambda d: d.find_element(By.ID, 'userid'))
+username = browser.find_element(By.ID, 'userid')
 username.send_keys(usernameStr)
-password = browser.find_element_by_id('pwd')
+password = browser.find_element(By.ID, 'pwd')
 password.send_keys(passwordStr)
 
-signInButton = browser.find_element_by_class_name('btn-primary')
+signInButton = browser.find_element(By.CLASS_NAME, 'btn-primary')
 signInButton.click()
 
-WebDriverWait(browser, 10).until(lambda d: d.find_element_by_id('SCC_LO_FL_WRK_SCC_VIEW_BTN$3'))
-time.sleep(5)
-shoppingCartButton = browser.find_element_by_id('SCC_LO_FL_WRK_SCC_VIEW_BTN$3')
+try:
+    WebDriverWait(browser, 5).until(lambda d: d.find_element(By.ID, 'SCC_LO_FL_WRK_SCC_VIEW_BTN$3'))
+except selenium.common.exceptions.TimeoutException:
+    print("Authentication details incorrect or server took too long to respond. Please try again later.")
+    exit(-1)
+
+WebDriverWait(browser, 5).until(lambda d: d.find_element(By.ID, 'SCC_LO_FL_WRK_SCC_VIEW_BTN$3'))
+shoppingCartButton = browser.find_element(By.ID, 'SCC_LO_FL_WRK_SCC_VIEW_BTN$3')
 shoppingCartButton.click()
 
-WebDriverWait(browser, 10).until(lambda d: d.find_element_by_id('DERIVED_SSR_FL_SSR_ENROLL_FL'))
-enrollButton = browser.find_element_by_id('DERIVED_SSR_FL_SSR_ENROLL_FL')
+WebDriverWait(browser, 10).until(lambda d: d.find_element(By.ID, 'DERIVED_SSR_FL_SSR_ENROLL_FL'))
+enrollButton = browser.find_element(By.ID, 'DERIVED_SSR_FL_SSR_ENROLL_FL')
 
-input("Please check all the classes that you want to register for. Then click ENTER.")
+input("Please check all the classes that you want to register for. Then press ENTER.")
 enrollButton.click()
-WebDriverWait(browser, 10).until(lambda d: d.find_element_by_id('#ICYes'))
-yesButton = browser.find_element_by_id('#ICYes')
+WebDriverWait(browser, 10).until(lambda d: d.find_element(By.ID, '#ICYes'))
+yesButton = browser.find_element(By.ID, '#ICYes')
 
-# Wait until its time
+# Wait until it's time
 while True:
     curr_time = datetime.datetime.now()
     time = "Waiting... " + curr_time.strftime('%H:%M:%S')
@@ -68,8 +78,10 @@ while True:
         pass
 
     if curr_time >= registration_time:
-        print("Executing")
+        print("Executing class registration.")
         yesButton.click()
         print("Successfully registered.")
         WebDriverWait(browser, 10000)
         break
+
+input("Click ENTER to exit.")
